fbd93279a55d4ccf1f2754a5fad519efd5c6d718
Caeldeth <sabrael@hybrasyl.com>
2021-09-11 09:32:46 -0500

    define error checking for generation options

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,40 @@
 // Assignment code here
-function makePass() {
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var nums = "1234567890";
-  var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"\\";
+
+// initialize variables for password generation
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var nums = "1234567890";
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"\\";
+
+// grab values of generator options
+const length = document.getElementById("char-num");
+const incLower = document.getElementById("char-lower");
+const incUpper = document.getElementById("char-upper");
+const incSpecial = document.getElementById ("char-special");
+const incNumber = document.getElementById ("char-numeric");
+
+function validateOptions () {
+  if ((128 > length < 8) || (char-lower + char-upper + char-special + char-numeric > 0)) {
+    console.log("Value of lowercase is: " && incLower);
+    console.log("Value of uppercase is: " && incUpper);
+    console.log("Value of special char is: " && incSpecial);
+    console.log("Value of numeric char is: " && incNumber);
+    console.log("Invalid value of checkboxes is: " && (char-lower + char-upper + char-special + char-numeric))
+    console.log("Value of length is:" && length);
+    return false;
+  } else {
+    console.log("Value of lowercase is: " && incLower);
+    console.log("Value of uppercase is: " && incUpper);
+    console.log("Value of special char is: " && incSpecial);
+    console.log("Value of numeric char is: " && incNumber);
+    console.log("Value of length is:" && length);
+    return true;
+  }
 }
+
+function generatePassword() {
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
